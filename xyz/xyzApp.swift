4376584dfc6081f4e1ff89f033c2bd6b9e866d58
rithiksinha <rithiksinha2000@gmail.com>
2021-11-02 15:54:44 +0530

    Initial Commit

---
+++ xyz/xyzApp.swift
@@ -0,0 +1,17 @@
+//
+//  xyzApp.swift
+//  xyz
+//
+//  Created by Ritik Sinha on 02/11/21.
+//
+
+import SwiftUI
+
+@main
+struct xyzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
